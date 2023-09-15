d1277824f5f6f36975cd6e4a4046c951e7ed61d8
tracer-pa-public <tracer@njuics.org>
2023-09-15 22:25:36 +0800

    > compile nemu testcase kernel > id 221220160 > user pa221220160 > uname Linux 8175718b922c 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 22:25:36 up 60 days,  9:12,  2 users,  load average: 2.75, 2.78, 2.86 > unitime 1694787936733 > ebb22103a3c69476c42efd92f93f77ba65e9894e

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -108,7 +108,7 @@ uint32_t alu_sub(uint32_t src, uint32_t dest, size_t data_size)
 	set_ZF(res, data_size);
 	set_SF(res, data_size);
 	set_OF_add(res, src, dest, data_size);
-	printf("res = %x\n", res);
+	printf("res = 0x%x\n", res);
     return res & (0xFFFFFFFF >> (32 - data_size)); // 高位清零
 #endif
 }

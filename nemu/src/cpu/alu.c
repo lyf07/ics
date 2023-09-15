d36f1a2a4cae7526277825f76c7475f9c3be9c99
tracer-pa-public <tracer@njuics.org>
2023-09-15 21:27:47 +0800

    > compile nemu testcase kernel > id 221220160 > user pa221220160 > uname Linux 8175718b922c 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 21:27:47 up 60 days,  8:14,  2 users,  load average: 2.95, 2.70, 2.69 > unitime 1694784467018 > f7934e21f0fb92ee4ce19b24638f870ecc5a4e3e

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -4,8 +4,8 @@ extern CPU_STATE cpu;
 
 
 void set_CF_add(uint32_t res, uint32_t src, size_t data_size) {
-    res = sign_ext(res & (0xFFFFFFFF >> (32 - data_size)));
-    src = sign_ext(src & (0xFFFFFFFF >> (32 - data_size)));
+    res = sign_ext(res & (0xFFFFFFFF >> (32 - data_size)), data_size);
+    src = sign_ext(src & (0xFFFFFFFF >> (32 - data_size)), data_size);
     cpu.eflags.CF = res < src;
 }
 

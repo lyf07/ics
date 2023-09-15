4ce51529e27b04029b45b60a240e96900e8c0940
tracer-pa-public <tracer@njuics.org>
2023-09-15 22:21:27 +0800

    > clean > id 221220160 > user pa221220160 > uname Linux 8175718b922c 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 22:21:27 up 60 days,  9:08,  2 users,  load average: 2.51, 2.75, 2.88 > unitime > c51aeeb9fd0cc8e8f89d7acecb16e19ad38bf6

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -101,8 +101,8 @@ uint32_t alu_sub(uint32_t src, uint32_t dest, size_t data_size)
 #ifdef NEMU_REF_ALU
 	return __ref_alu_sub(src, dest, data_size);
 #else
-    uint32_t temp = take_reverse(src, data_size);
-    uint32_t res = temp + dest + cpu.eflags.CF;
+    uint32_t temp = take_reverse(dest, data_size);
+    uint32_t res = src + temp + cpu.eflags.CF;
     set_CF_sub(res, src, data_size);   // 设置标志位
 	set_PF(res);                       // 偶数个1时，置1 
 	set_ZF(res, data_size);

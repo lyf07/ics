8381addbb76a60fcd1d318d3924274bf611bf6d9
tracer-pa-public <tracer@njuics.org>
2023-09-15 21:29:52 +0800

    > compile nemu testcase kernel > id 221220160 > user pa221220160 > uname Linux 8175718b922c 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 21:29:52 up 60 days,  8:16,  2 users,  load average: 2.60, 2.68, 2.69 > unitime 1694784592408 > c3a12beba2c4e97f1e743b6ce4d768d70ecb9

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -1,5 +1,6 @@
 #include "../../include/cpu/cpu.h"
 #include "../../include/cpu/alu.h"
+#include <iostream>
 extern CPU_STATE cpu;
 
 
@@ -12,12 +13,14 @@ void set_CF_add(uint32_t res, uint32_t src, size_t data_size) {
 void set_PF(uint32_t res) {
     int cnt1 = 8, cnt2 = 0;
     uint8_t temp = sign_ext(res, 8);
+    std::cout << "res = " << res << endl;
     while (cnt1--) {
         uint8_t temp2 = temp << cnt1;
         if (sign(temp2)) {
             cnt2++;
         }
     }
+    std
     cpu.eflags.PF = cnt2 % 2 ? 0 : 1;
 }
 

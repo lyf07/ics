d27140cf1d149cdc810e4feef047afd2bac0a4ea
tracer-pa-public <tracer@njuics.org>
2023-09-15 22:58:10 +0800

    > clean > id 221220160 > user pa221220160 > uname Linux 8175718b922c 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 22:58:10 up 60 days,  9:44,  2 users,  load average: 2.98, 2.94, 2.97 > unitime > 1e2161557ef9176c4f30d954c21599aae12e14cc

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -103,7 +103,7 @@ uint32_t alu_sub(uint32_t src, uint32_t dest, size_t data_size)
 	return __ref_alu_sub(src, dest, data_size);
 #else
     uint32_t res = src - dest;
-    set_CF_add(res, src, data_size);   // 设置标志位
+    set_CF_sub(res, src, data_size);   // 设置标志位
 	set_PF(res);                       // 偶数个1时，置1 
 	set_ZF(res, data_size);
 	set_SF(res, data_size);

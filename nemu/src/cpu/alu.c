0a0e0507473b7e9093ab1a2ac22a5b8992008b4a
tracer-pa-public <tracer@njuics.org>
2023-09-15 22:14:28 +0800

    > compile nemu testcase kernel > id 221220160 > user pa221220160 > uname Linux 8175718b922c 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 22:14:28 up 60 days,  9:01,  2 users,  load average: 2.85, 2.93, 2.97 > unitime 1694787268767 > 93e61998a26a851adc8d74b98d42916c493bab3b

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -96,7 +96,7 @@ uint32_t alu_sub(uint32_t src, uint32_t dest, size_t data_size)
 #ifdef NEMU_REF_ALU
 	return __ref_alu_sub(src, dest, data_size);
 #else
-    uint32_t temp = take_reverse(src);
+    uint32_t temp = take_reverse(src, data_size);
     uint32_t res = temp + dest + cpu.eflags.CF;
     set_CF_adc(res, src, data_size);   // 设置标志位
 	set_PF(res);                       // 偶数个1时，置1 

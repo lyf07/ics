5f9e53dc22f9053ece9d532550648e63dc9c10c7
tracer-pa-public <tracer@njuics.org>
2023-09-15 22:27:59 +0800

    > clean > id 221220160 > user pa221220160 > uname Linux 8175718b922c 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 22:27:59 up 60 days,  9:14,  2 users,  load average: 3.74, 3.30, 3.04 > unitime > 2e8c4c6facfb9fae873ecbed8a3d6b77ac06dcce

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -100,6 +100,7 @@ uint32_t alu_adc(uint32_t src, uint32_t dest, size_t data_size)
 uint32_t alu_sub(uint32_t src, uint32_t dest, size_t data_size)
 {
 #ifdef NEMU_REF_ALU
+    printf( "answer = 0x%x", __ref_alu_sub(src, dest, data_size))
 	return __ref_alu_sub(src, dest, data_size);
 #else
     uint32_t temp = take_reverse(dest, data_size);

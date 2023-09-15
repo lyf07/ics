378f992dd7cb772d380df62294d2af94bbdfa31e
tracer-pa-public <tracer@njuics.org>
2023-09-15 22:18:17 +0800

    > clean > id 221220160 > user pa221220160 > uname Linux 8175718b922c 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 22:18:17 up 60 days,  9:05,  2 users,  load average: 2.70, 2.80, 2.91 > unitime > 8629e9e3909f611eaf1c34cba43549e84d7bbb5

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -45,11 +45,16 @@ void set_OF_add(uint32_t res, uint32_t src, uint32_t dest, size_t data_size) {
     }
 }
 
-
 void set_CF_adc(uint32_t res, uint32_t src, size_t data_size) {
     res = sign_ext(res & (0xFFFFFFFF >> (32 - data_size)), data_size);
     src = sign_ext(src & (0xFFFFFFFF >> (32 - data_size)), data_size);
     cpu.eflags.CF = (cpu.eflags.CF == 1) ? res <= src : res < src;
+}
+
+void set_CF_sub(uint32_t res, uint32_t src, size_t data_size) {
+    res = sign_ext(res & (0xFFFFFFFF >> (32 - data_size)), data_size);
+    src = sign_ext(src & (0xFFFFFFFF >> (32 - data_size)), data_size);
+    cpu.eflags.CF = res > src;
 }
 
 uint32_t take_reverse(uint32_t num, size_t data_size) {

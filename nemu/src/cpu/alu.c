82f991a0ca6823947e0fccfc2c6953448b4c486d
tracer-pa-public <tracer@njuics.org>
2023-09-16 09:11:40 +0800

    > clean > id 221220160 > user pa221220160 > uname Linux 8175718b922c 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 09:11:40 up 60 days, 19:58,  2 users,  load average: 3.19, 3.00, 2.86 > unitime > 4b83894e20b78ae91dbde93a5c759a626eee91

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -102,9 +102,9 @@ uint32_t alu_sub(uint32_t src, uint32_t dest, size_t data_size)
 #ifdef NEMU_REF_ALU
 	return __ref_alu_sub(src, dest, data_size);
 #else
-    uint32_t res = src - dest;
+    uint32_t res = dest - src;
     printf("0:res = 0x%x\n", res);
-    set_CF_sub(res, src, data_size);   // 设置标志位
+    set_CF_sub(res, dest, data_size);   // 设置标志位
     printf("1:res = 0x%x\n", res);
 	set_PF(res);                       // 偶数个1时，置1 
 	set_ZF(res, data_size);
@@ -113,6 +113,7 @@ uint32_t alu_sub(uint32_t src, uint32_t dest, size_t data_size)
     return res & (0xFFFFFFFF >> (32 - data_size)); // 高位清零
 #endif
 }
+
 
 uint32_t alu_sbb(uint32_t src, uint32_t dest, size_t data_size)
 {

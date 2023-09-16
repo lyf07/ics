c11fd148ddf96499c60d8a4aa5274b83a1636525
tracer-pa-public <tracer@njuics.org>
2023-09-16 09:21:12 +0800

    > clean > id 221220160 > user pa221220160 > uname Linux 8175718b922c 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 09:21:12 up 60 days, 20:07,  2 users,  load average: 2.84, 2.99, 2.93 > unitime > 7b828776d73785bdc1b7a240607b2df3606f1309

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -73,7 +73,7 @@ void set_CF_adc(uint32_t res, uint32_t src, size_t data_size) {
     res = sign_ext(res & (0xFFFFFFFF >> (32 - data_size)), data_size);
     src = sign_ext(src & (0xFFFFFFFF >> (32 - data_size)), data_size);
     cpu.eflags.CF = (cpu.eflags.CF == 1) ? res <= src : res < src;
-
+}
 
 void set_CF_sub(uint32_t res, uint32_t src, size_t data_size) {
     res = sign_ext(res & (0xFFFFFFFF >> (32 - data_size)), data_size);

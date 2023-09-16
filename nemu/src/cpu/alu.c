231c6b4c6b4a53b9de4ae3f8fa0e678f3499c014
tracer-pa-public <tracer@njuics.org>
2023-09-16 10:49:30 +0800

    > compile nemu testcase kernel > id 221220160 > user pa221220160 > uname Linux 8175718b922c 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 10:49:30 up 60 days, 21:36,  2 users,  load average: 3.14, 2.91, 2.92 > unitime 1694832570580 > 769649a0f50396a19f9d2c75c0e2f05ca37e650c

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -327,20 +327,18 @@ uint32_t alu_sar(uint32_t src, uint32_t dest, size_t data_size)
     int32_t temp = dest << (32 - data_size);
     temp = temp >> src;
     uint32_t temp2 = (uint32_t)temp >> (32 - data_size);
-    uint32_t res = dest & (0xFFFFFFFF << data_size);
     printf("src = 0x%x\n", src);
     printf("dest = 0x%x\n", dest);
     printf("data_size = 0x%x\n", data_size);
     printf("temp = 0x%x\n", temp);
     printf("temp2 = 0x%x\n", temp2);
     printf("0: res = 0x%x\n", res);
-    res += temp2;
     set_CF_mvr(res, src, dest, data_size);
     set_PF(res);                       // 偶数个1时，置1 
 	set_ZF(res, data_size);
 	set_SF(res, data_size);
 	printf("1: res = 0x%x\n", res);
-    return res & (0xFFFFFFFF >> (32 - data_size));
+    return temp32;
 #endif
 }
 

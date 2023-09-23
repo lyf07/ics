36f1d4d17967a3deb4425b23dc2a02f2a1c8f27b
tracer-pa-public <tracer@njuics.org>
2023-09-23 16:24:31 +0800

    > compile nemu testcase kernel > id 221220160 > user pa221220160 > uname Linux 8175718b922c 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 16:24:31 up 68 days,  3:11,  2 users,  load average: 3.20, 3.15, 3.11 > unitime 1695457471018 > be2ff5bc3335716466c1518d5a5414b2502570cb

--- nemu/src/cpu/fpu.c
+++ nemu/src/cpu/fpu.c
@@ -158,11 +158,6 @@ uint32_t internal_float_add(uint32_t b, uint32_t a)
 	uint32_t shift = fb.exponent - fa.exponent - 1;
 
 	/* TODO: shift = ? */
-	printf("\e[0;31mPlease implement me at fpu.c\e[0m\n");
-	fflush(stdout);
-	assert(0);
-	assert(shift >= 0);
-
 	sig_a = (sig_a << 3); // guard, round, sticky
 	sig_b = (sig_b << 3);
 

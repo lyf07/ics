fd1c5609617b79623f44911120d5907595cb295e
tracer-pa-public <tracer@njuics.org>
2023-09-23 17:57:36 +0800

    > compile nemu testcase kernel > id 221220160 > user pa221220160 > uname Linux 8175718b922c 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 17:57:36 up 68 days,  4:44,  2 users,  load average: 2.46, 2.77, 2.89 > unitime 1695463056452 > 145f6986ca7b3888c6fa3a6c8f4728bcae53e11a

--- nemu/src/cpu/fpu.c
+++ nemu/src/cpu/fpu.c
@@ -24,8 +24,8 @@ inline uint32_t internal_normalize(uint32_t sign, int32_t exp, uint64_t sig_grs)
 
 			/* TODO: shift right, pay attention to sticky bit*/
 			sticky = sticky | (sig_grs & 0x1);
-			sig_args >>= 1;
-			sig_args |= sticky;
+			sig_grs >>= 1;
+			sig_grs |= sticky;
 		}
 
 		if (exp >= 0xff)
@@ -62,9 +62,9 @@ inline uint32_t internal_normalize(uint32_t sign, int32_t exp, uint64_t sig_grs)
 		{
 			// denormal
 			/* TODO: shift right, pay attention to sticky bit*/
-            sticky = sig_args & 0x1;
-            sig_args >>= 1;
-            sig_args |= sticky;
+            sticky = sig_grs & 0x1;
+            sig_grs >>= 1;
+            sig_grs |= sticky;
 		}
 	}
 	else if (exp == 0 && sig_grs >> (23 + 3) == 1)
@@ -96,7 +96,7 @@ inline uint32_t internal_normalize(uint32_t sign, int32_t exp, uint64_t sig_grs)
             if (exp >= 0xff) {   // 无穷大
                  return sign ?  (0xff000000 >> 1 | 0x80000000) : (0xff000000 >> 1);
             }
-            sig_args &= 0x70000; // 丢弃隐藏的最高位
+            sig_grs &= 0x70000; // 丢弃隐藏的最高位
         }
 	}
 

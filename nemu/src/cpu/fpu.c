9cdb364232bd0c92c7bfa0816a49dbc49eec3d94
tracer-pa-public <tracer@njuics.org>
2023-09-23 18:18:47 +0800

    > compile nemu testcase kernel > id 221220160 > user pa221220160 > uname Linux 8175718b922c 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 18:18:47 up 68 days,  5:05,  2 users,  load average: 2.45, 2.82, 2.92 > unitime 1695464327774 > c94458255427bfd5b41c4b4dc466dcb41c48f4

--- nemu/src/cpu/fpu.c
+++ nemu/src/cpu/fpu.c
@@ -12,7 +12,7 @@ inline uint32_t internal_normalize(uint32_t sign, int32_t exp, uint64_t sig_grs)
 	// normalization
 	bool overflow = false; // true if the result is INFINITY or 0 during normalize
 	uint32_t sticky = 0;
-    printf("sign = 0x%u, exp = 0x%u, sig_grs = 0x%llx\n", sign, exp, sig_grs);
+    printf("0: sign = 0x%u, exp = 0x%u, sig_grs = 0x%llx\n", sign, exp, sig_grs);
 	if ((sig_grs >> (23 + 3)) > 1 || exp < 0)
 	{
 		// normalize toward right
@@ -27,7 +27,7 @@ inline uint32_t internal_normalize(uint32_t sign, int32_t exp, uint64_t sig_grs)
 			sig_grs >>= 1;
 			sig_grs |= sticky;
 		}
-
+        printf("1: sign = 0x%u, exp = 0x%u, sig_grs = 0x%llx, sticky = 0x%x\n", sign, exp, sig_grs, sticky);
 		if (exp >= 0xff)
 		{
 			/* TODO: assign the number to infinity */

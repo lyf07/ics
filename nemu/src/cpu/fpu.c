5fa2285c0c771faeb114a12387c1cbf4f4412fea
tracer-pa-public <tracer@njuics.org>
2023-09-23 19:09:54 +0800

    > compile nemu testcase kernel > id 221220160 > user pa221220160 > uname Linux 8175718b922c 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 19:09:54 up 68 days,  5:56,  2 users,  load average: 3.43, 3.28, 3.07 > unitime 1695467394029 > f0a0467536a94690912c296f4f8b5d7323e5dd31

--- nemu/src/cpu/fpu.c
+++ nemu/src/cpu/fpu.c
@@ -139,7 +139,11 @@ CORNER_CASE_RULE corner_add[] = {
 // a + b
 uint32_t internal_float_add(uint32_t b, uint32_t a)
 {
-
+    FLOAT x, y, g;
+    x.val = a;
+    y.val = b;
+    g.fval = x.fval + y.fval;
+    return g.val;
     
     
     

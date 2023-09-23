2a5bad9ec8365def6fbb1a618b921844aa3ccb19
tracer-pa-public <tracer@njuics.org>
2023-09-23 21:19:50 +0800

    > compile nemu testcase kernel > id 221220160 > user pa221220160 > uname Linux 8175718b922c 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 21:19:50 up 68 days,  8:06,  2 users,  load average: 2.61, 2.85, 2.93 > unitime 1695475190377 > 791d2af074413e2ff98dd86dec6aa85e7b0b1861

--- nemu/src/cpu/fpu.c
+++ nemu/src/cpu/fpu.c
@@ -306,7 +306,7 @@ uint32_t internal_float_mul(uint32_t b, uint32_t a)
 	uint32_t exp_res = 0;
 
 	/* TODO: exp_res = ? leave space for GRS bits. */
-    exp_res = fa.exponent + fb.exponent - 127 - 23;
+    exp_res = fa.exponent + fb.exponent - 127 - 23 + 3;
     // FLOAT c, d, e;
     // c.val = b, d.val = a;
     // e.fval = c.fval * d.fval;

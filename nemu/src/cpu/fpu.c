8e22b7ecdd41a3dd97268d8927d5e39c0f76b8e0
tracer-pa-public <tracer@njuics.org>
2023-09-23 18:35:57 +0800

    > compile nemu testcase kernel > id 221220160 > user pa221220160 > uname Linux 8175718b922c 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 18:35:57 up 68 days,  5:22,  2 users,  load average: 3.15, 3.21, 3.03 > unitime 1695465357076 > 4a38fcfd8985249cc3564875d305a0ad6d31b780

--- nemu/src/cpu/fpu.c
+++ nemu/src/cpu/fpu.c
@@ -27,6 +27,7 @@ inline uint32_t internal_normalize(uint32_t sign, int32_t exp, uint64_t sig_grs)
 			sticky = sticky | (sig_grs & 0x1);
 			sig_grs >>= 1;
 			sig_grs |= sticky;
+			exp--;
 		}
 		if (exp >= 0xff)
 		{
@@ -58,6 +59,7 @@ inline uint32_t internal_normalize(uint32_t sign, int32_t exp, uint64_t sig_grs)
 		while (((sig_grs >> (23 + 3)) == 0) && exp > 0)
 		{
 			sig_grs <<= 1;
+			exp++;
 		}
 		if (exp == 0)
 		{
@@ -66,6 +68,7 @@ inline uint32_t internal_normalize(uint32_t sign, int32_t exp, uint64_t sig_grs)
             sticky = sig_grs & 0x1;
             sig_grs >>= 1;
             sig_grs |= sticky;
+            exp++;
 		}
 	}
 	else if (exp == 0 && sig_grs >> (23 + 3) == 1)

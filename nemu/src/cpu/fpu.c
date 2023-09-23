e394604541884a56d3da4c0c3e44ca1cfaf37cbd
tracer-pa-public <tracer@njuics.org>
2023-09-23 21:05:32 +0800

    > compile nemu testcase kernel > id 221220160 > user pa221220160 > uname Linux 8175718b922c 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 21:05:32 up 68 days,  7:52,  2 users,  load average: 2.83, 2.87, 2.95 > unitime 1695474332728 > 9152b717c12e1cda2ee261227c9a370b4c1ec119

--- nemu/src/cpu/fpu.c
+++ nemu/src/cpu/fpu.c
@@ -104,7 +104,7 @@ inline uint32_t internal_normalize(uint32_t sign, int32_t exp, uint64_t sig_grs)
 	f.sign = sign;
 	f.exponent = (uint32_t)(exp & 0xff);
 	f.fraction = sig_grs; // here only the lowest 23 bits are kept
-	printf("My answer is 0x%x\n", f.val);
+// 	printf("My answer is 0x%x\n", f.val);
 	printf("===============\n");
 	return f.val;
 }

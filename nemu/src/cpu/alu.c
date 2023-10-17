7557289f79001f5bf289bff5b5d150e29d8ca611
tracer-pa-public <tracer@njuics.org>
2023-10-17 23:42:28 +0800

    > compile nemu testcase kernel > id 221220160 > user pa221220160 > uname Linux 8175718b922c 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 23:42:28 up 92 days, 10:29,  4 users,  load average: 2.62, 2.74, 2.74 > unitime 1697557347956 > 446b0ab99239564d238d44ee90e767c3ff878a22

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -22,7 +22,6 @@ void set_ZF(uint32_t res, size_t data_size) {
     res = res & (0xFFFFFFFF >> (32 - data_size));
     printf("in zf, res = %x\n", res);
     cpu.eflags.ZF = (res == 0);
-    printf("in zf, zf = %d\n",cpu.eflags.ZF);
 }
 	
 void set_SF(uint32_t res, size_t data_size) {
@@ -299,6 +298,7 @@ uint32_t alu_and(uint32_t src, uint32_t dest, size_t data_size)
 	set_SF(res, data_size);
 	printf("sf = %d\n",cpu.eflags.SF);
 	printf("Of, of = %d\n",cpu.eflags.OF);
+	printf("in zf, zf = %d\n",cpu.eflags.ZF);
 	return res & (0xFFFFFFFF >> (32 - data_size));
 #endif
 }

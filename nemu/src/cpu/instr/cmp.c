0a493aacb56ff84cac4f6dc050fc33ef7daaced3
tracer-pa-public <tracer@njuics.org>
2023-10-17 21:11:41 +0800

    > compile nemu testcase kernel > id 221220160 > user pa221220160 > uname Linux 8175718b922c 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 21:11:41 up 92 days,  7:58,  4 users,  load average: 3.16, 3.13, 2.97 > unitime 1697548301790 > 1fc0b5d6bd8868ed083e6dd56307cba2fcd643a

--- nemu/src/cpu/instr/cmp.c
+++ nemu/src/cpu/instr/cmp.c
@@ -7,7 +7,7 @@ static void instr_execute_2op()
 {
 	operand_read(&opr_src);
 	printf("src = %x\n",sign_ext(opr_src.val, data_size));
-	printf("ori_dest = %x", opr_dest.val);
+	printf("ori_dest = %x\n", opr_dest.val);
 	printf("dest = %x\n", sign_ext(opr_dest.val, data_size));
 	alu_sub(sign_ext(opr_src.val, data_size), sign_ext(opr_dest.val, data_size), data_size);
 	operand_write(&opr_dest);

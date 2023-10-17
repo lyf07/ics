44bbf48ebbe350b6def1b0e0619f22ce73dabc06
tracer-pa-public <tracer@njuics.org>
2023-10-17 22:50:57 +0800

    > compile nemu testcase kernel > id 221220160 > user pa221220160 > uname Linux 8175718b922c 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 22:50:57 up 92 days,  9:37,  5 users,  load average: 3.05, 2.86, 2.84 > unitime 1697554257385 > af83738afc8506a63333c093e0f11ed99f5439ac

--- nemu/src/cpu/instr/test.c
+++ nemu/src/cpu/instr/test.c
@@ -5,6 +5,8 @@ Put the implementations of `test' instructions here.
 static void instr_execute_2op() 
 {
 	operand_read(&opr_src);
+	printf("opr_src: %u\n", opr_src.val);
+	printf("opr_test: %u\n\n", opr_dest.val);
 	alu_and(opr_src.val, opr_dest.val, opr_src.data_size);
 	operand_write(&opr_dest);
 }

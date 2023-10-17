cf4fa3177dadf1ddec1370c5ee728a1677501f00
tracer-pa-public <tracer@njuics.org>
2023-10-17 20:49:10 +0800

    > compile nemu testcase kernel > id 221220160 > user pa221220160 > uname Linux 8175718b922c 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 20:49:10 up 92 days,  7:35,  4 users,  load average: 2.51, 2.66, 2.75 > unitime 1697546950566 > bf111926ea1e4709ab7a4e94a9396a105c1849

--- nemu/src/cpu/instr/cmp.c
+++ nemu/src/cpu/instr/cmp.c
@@ -2,13 +2,40 @@
 /*
 Put the implementations of `cmp' instructions here.
 */
-
-static void instr_execute_2op() 
+static void instr_execute_2op()
 {
-	operand_read(&opr_src);
-	alu_sub(sign_ext(opr_src.val, data_size), sign_ext(opr_dest.val, data_size), data_size);
-	operand_write(&opr_dest);
+    operand_read(&opr_src);
+    operand_read(&opr_dest);
+    //printf("%x\t%x\n",opr_src.val,opr_dest.val);
+    #if 1//8位src需要先ext
+        opr_src.val = sign_ext(opr_src.val, opr_src.data_size);
+    #endif
+    
+    #if 1
+        alu_sub(opr_src.val, opr_dest.val, opr_dest.data_size);//alu_sub是"2参-1参"，cmp是"dest-src"
+        //printf("%x\t%x\n",cpu.eflags.ZF,cpu.eflags.CF);
+        //printf("jl(signed):sf%d\tof%d\n", cpu.eflags.SF, cpu.eflags.OF);
+    #elif 0
+        if(opr_src.val == opr_dest.val){
+            cpu.eflags.ZF = 1;
+        }
+        else if(opr_dest.val < opr_src.val){
+            cpu.eflags.ZF = 0;
+            cpu.eflags.CF = 1;//借位
+        }
+        else if(opr_dest.val > opr_src.val){
+            cpu.eflags.ZF = 0;
+            cpu.eflags.CF = 0;
+        }
+    #endif
 }
 
 make_instr_impl_2op(cmp, i, rm, bv)
-make_instr_impl_2op(cmp, i, r, b)
+make_instr_impl_2op(cmp, i, rm, v)
+make_instr_impl_2op(cmp, i, a, b)
+make_instr_impl_2op(cmp, i, a, v)
+make_instr_impl_2op(cmp, i, rm, b)
+make_instr_impl_2op(cmp, rm, r, v)
+make_instr_impl_2op(cmp, rm, r, b)
+make_instr_impl_2op(cmp, r, rm, b)
+make_instr_impl_2op(cmp, r, rm, v)

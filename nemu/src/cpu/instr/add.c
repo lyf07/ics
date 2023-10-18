29fae2c72cba403e2db21fb999f6d01c12ec0b1f
tracer-pa-public <tracer@njuics.org>
2023-10-18 13:58:24 +0800

    > compile nemu testcase kernel > id 221220160 > user pa221220160 > uname Linux 8175718b922c 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 13:58:24 up 93 days, 45 min,  4 users,  load average: 2.96, 2.87, 2.82 > unitime 1697608704637 > ed73f774beea98cef47a5fb2d16aa41a0251cf5

--- nemu/src/cpu/instr/add.c
+++ nemu/src/cpu/instr/add.c
@@ -6,7 +6,7 @@ static void instr_execute_2op()
 {
 	operand_read(&opr_src);
 	printf("opr_src.val = 0x%x, opr_dext.val = 0x%x\n", opr_src.val, opr_dest.val);
-	opr_dest.val = alu_add(sign_ext(opr_src.val, opr_src.data_size), sign_ext(opr_dest.val, opr_dest.data_size), opr_src.data_size);
+	opr_dest.val = alu_add(sign_ext(opr_src.val, opr_src.data_size), sign_ext(opr_dest.val, opr_dest.data_size), opr_dest.data_size);
 	printf("val = 0x%x\n\n\n", opr_dest.val);
 	operand_write(&opr_dest);
 }

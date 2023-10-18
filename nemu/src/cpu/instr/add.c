ef869f31f9d8563942d32e681a8695246381197f
tracer-pa-public <tracer@njuics.org>
2023-10-18 14:12:20 +0800

    > compile nemu testcase kernel > id 221220160 > user pa221220160 > uname Linux 8175718b922c 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 14:12:20 up 93 days, 59 min,  4 users,  load average: 2.72, 3.04, 2.91 > unitime 1697609539929 > c552c5fc65e9fff952d64ce24d480a2793c57821

--- nemu/src/cpu/instr/add.c
+++ nemu/src/cpu/instr/add.c
@@ -4,13 +4,12 @@ Put the implementations of `add' instructions here.
 */
 static void instr_execute_2op() 
 {
-	 operand_read(&opr_src);
-    operand_read(&opr_dest);
-    //opr_dest.val += opr_src.val;
-    opr_src.val = sign_ext(opr_src.val, opr_src.data_size);
-    opr_dest.val = alu_add(opr_dest.val, opr_src.val, opr_dest.data_size);
-	operand_write(&opr_dest);//ebp的值存到esp指向的内存区域
-
+	operand_read(&opr_src);
+	operand_read(&opr_dest);
+	printf("opr_src.val = 0x%x, opr_dest.val = 0x%x\n", opr_src.val, opr_dest.val);
+	opr_dest.val = alu_add(sign_ext(opr_src.val, opr_src.data_size), sign_ext(opr_dest.val, opr_dest.data_size), opr_dest.data_size);
+	printf("val = 0x%x\n\n\n", opr_dest.val);
+	operand_write(&opr_dest);
 }
 
 make_instr_impl_2op(add, r, rm, v)
--- nemu/src/cpu/instr/inc.c
+++ nemu/src/cpu/instr/inc.c
@@ -4,7 +4,7 @@ Put the implementations of `inc' instructions here.
 */
 static void instr_execute_1op(){
     operand_read(&opr_src);
-    opr_src.val++;
+    opr_src.val = alu_add(1, opr_src.val, data_size);
     operand_write(&opr_src);
 }
 

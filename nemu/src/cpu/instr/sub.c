3249a552b1add023a907d210750a75ec7973c8a0
tracer-pa-public <tracer@njuics.org>
2023-10-18 14:45:01 +0800

    > compile nemu testcase kernel > id 221220160 > user pa221220160 > uname Linux 8175718b922c 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 14:45:01 up 93 days,  1:31,  4 users,  load average: 2.62, 2.66, 2.76 > unitime 1697611500902 > 11a64e991cb9cdf7b537d1e66d8692ea70acb1b2

--- nemu/src/cpu/instr/sub.c
+++ nemu/src/cpu/instr/sub.c
@@ -5,9 +5,9 @@ Put the implementations of `sub' instructions here.
 static void instr_execute_2op() 
 {
 	operand_read(&opr_src);
-	operand_read(&opr_dest);
+// 	operand_read(&opr_dest);
 	printf("src = 0x%x, dest = 0x%x\n", opr_src.val, opr_dest.val);
-	alu_sub(sign_ext(opr_src.val, opr_src.data_size), sign_ext(opr_dest.val, opr_dest.data_size), opr_dest.data_size);
+	alu_sub(sign_ext(opr_src.val, opr_src.data_size), sign_ext(opr_dest.val, opr_dest.data_size), opr_src.data_size);
 }
 
 make_instr_impl_2op(sub, i, rm, b)

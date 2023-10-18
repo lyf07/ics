ad0591289400b23decbd5084d1167b3799b1aee8
tracer-pa-public <tracer@njuics.org>
2023-10-18 13:48:24 +0800

    > compile nemu testcase kernel > id 221220160 > user pa221220160 > uname Linux 8175718b922c 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 13:48:24 up 93 days, 35 min,  4 users,  load average: 2.78, 2.73, 2.79 > unitime 1697608104083 > 64f1892c124980cd188228227454104183e96604

--- nemu/src/cpu/instr/add.c
+++ nemu/src/cpu/instr/add.c
@@ -5,8 +5,9 @@ Put the implementations of `add' instructions here.
 static void instr_execute_2op() 
 {
 	operand_read(&opr_src);
+	printf("opr_src.val = 0x%x, opr_dext.val = 0x%x\n", opr_src.val, opr_dest.val);
 	opr_dest.val = alu_add(sign_ext(opr_src.val, opr_src.data_size), sign_ext(opr_dest.val, opr_dest.data_size), opr_src.data_size);
-	printf("val = 0x%x\n", opr_dest.val);
+	printf("val = 0x%x\n\n\n", opr_dest.val);
 	operand_write(&opr_dest);
 }
 

27722d9fde4b6a92cbd457bd934d1943ca669a90
tracer-pa-public <tracer@njuics.org>
2023-10-18 14:52:53 +0800

    > compile nemu testcase kernel > id 221220160 > user pa221220160 > uname Linux 8175718b922c 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 14:52:53 up 93 days,  1:39,  4 users,  load average: 2.56, 2.88, 2.87 > unitime 1697611973100 > e163afa69b80dac72f4cfc1f48db4e8986c2be9f

--- nemu/src/cpu/instr/cmp.c
+++ nemu/src/cpu/instr/cmp.c
@@ -6,10 +6,8 @@ Put the implementations of `cmp' instructions here.
 static void instr_execute_2op() 
 {
 	operand_read(&opr_src);
-// 	operand_read(&opr_dest);
-// 	printf("before: dest = 0x%x\n", opr_dest.val);
+	operand_read(&opr_dest);
 	alu_sub(sign_ext(opr_src.val, opr_src.data_size), sign_ext(opr_dest.val, opr_dest.data_size), opr_src.data_size);
-// 	printf("after: dest = 0x%x\n", opr_dest.val);
 }
 
 make_instr_impl_2op(cmp, i, rm, bv)

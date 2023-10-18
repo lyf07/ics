28084a8d06e6d5858ee38ceed40c9f1cb3bff622
tracer-pa-public <tracer@njuics.org>
2023-10-18 10:24:09 +0800

    > compile nemu testcase kernel > id 221220160 > user pa221220160 > uname Linux 8175718b922c 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 10:24:09 up 92 days, 21:10,  4 users,  load average: 2.76, 2.72, 2.79 > unitime 1697595849022 > c1ff2d2342c00c5fa04e13e9c037c55f2ecab924

--- nemu/src/cpu/instr/add.c
+++ nemu/src/cpu/instr/add.c
@@ -2,3 +2,11 @@
 /*
 Put the implementations of `add' instructions here.
 */
+static void instr_execute_2op() 
+{
+	operand_read(&opr_src);
+	alu_add(sign_ext(opr_src.val, opr_src.data_size), sign_ext(opr_dest.val, opr_dest.data_size), opr_src.data_size);
+	operand_write(&opr_dest);
+}
+
+make_instr_impl_2op(add, r, rm, v)

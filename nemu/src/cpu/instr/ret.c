2ff4fb9b8c450f4426338899d4c4308ad89fa9f3
tracer-pa-public <tracer@njuics.org>
2023-10-20 17:11:33 +0800

    > compile nemu testcase kernel > id 221220160 > user pa221220160 > uname Linux 8175718b922c 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 17:11:33 up 95 days,  3:58,  4 users,  load average: 1.32, 1.61, 1.68 > unitime 1697793093095 > 2794b3bec9c858df8401c54b5e5a809da3ed478a

--- nemu/src/cpu/instr/ret.c
+++ nemu/src/cpu/instr/ret.c
@@ -6,6 +6,7 @@ make_instr_func(ret) {
     OPERAND opr;
     opr.addr = cpu.esp;
     opr.type = OPR_MEM;
+    cpu.esp += 4;//push和pop都是按32位
     opr.data_size = 32;
     operand_read(&opr);
     cpu.eip = opr.val - 1;

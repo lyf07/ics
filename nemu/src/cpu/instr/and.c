c62af4a6ad52e537974d3159332cf726c283ff69
tracer-pa-public <tracer@njuics.org>
2023-10-20 19:31:59 +0800

    > compile nemu testcase kernel > id 221220160 > user pa221220160 > uname Linux 8175718b922c 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 19:31:59 up 95 days,  6:18,  4 users,  load average: 1.38, 1.57, 1.69 > unitime 1697801519131 > 133a583a7249d4c4e346d24c387338cefe89a799

--- nemu/src/cpu/instr/and.c
+++ nemu/src/cpu/instr/and.c
@@ -2,3 +2,13 @@
 /*
 Put the implementations of `and' instructions here.
 */
+static void instr_execute_2op() 
+{
+	operand_read(&opr_src);
+	operand_read(&opr_dest);
+	opr_dest.val = alu_and(opr_src.val, sign_ext(opr_dest.val, opr_dest.data_size), opr_src.data_size);
+	operand_write(&opr_dest);
+}
+
+
+make_instr_impl_2op(add, i, rm, bv)

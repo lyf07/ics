837d07669a9c0b19bf72a91e7b1975cde7ad4507
tracer-pa-public <tracer@njuics.org>
2023-10-20 17:09:54 +0800

    > compile nemu testcase kernel > id 221220160 > user pa221220160 > uname Linux 8175718b922c 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 17:09:54 up 95 days,  3:56,  4 users,  load average: 1.41, 1.68, 1.71 > unitime 1697792994454 > 3a122430a923e728cb99c854398fc7b5f14272f9

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -3,24 +3,19 @@
 Put the implementations of `call' instructions here.
 */
 make_instr_func(call_i_near) {
-      // 5 = 1(call's opcode) + 4(16 immediate num)
     
-//push eip
-        int len = 1;
-        len += 32 / 8;
-       	cpu.esp -= 4;
-	    paddr_write(cpu.esp, 4, cpu.eip + 5); 
-    //读取目标地址
-        OPERAND opr_src;
-        opr_src.type = OPR_IMM;
-        opr_src.sreg = SREG_CS;
-        opr_src.data_size = data_size;
-        opr_src.addr = eip + 1;
-        operand_read(&opr_src);
-    //跳转
-        int offset = sign_ext(opr_src.val, data_size); //读4位
-        print_asm_1("call_near", "", len, &opr_src);
-        cpu.eip += offset;
-    return len;
-
+	cpu.esp -= 4;
+	paddr_write(cpu.esp, 4, cpu.eip + 5);   // 5 = 1(call's opcode) + 4(16 immediate)
+    
+    
+    OPERAND opr;
+    opr.type = OPR_IMM;
+    opr.addr = cpu.eip + 1;
+    opr.data_size = 16;
+    operand_read(&opr);
+    
+    cpu.eip += sign_ext(opr.val, opr.data_size);
+    
+    return 5;
+    
 }

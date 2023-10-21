90a9bce09d93343195827c64385abbaf089a2a8e
tracer-pa-public <tracer@njuics.org>
2023-10-21 15:43:03 +0800

    > compile nemu testcase kernel > id 221220160 > user pa221220160 > uname Linux 8175718b922c 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 15:43:03 up 96 days,  2:29,  4 users,  load average: 1.80, 1.66, 1.64 > unitime 1697874183849 > 56ee6b4e8d11002ed1e5c0ee80d86e21d8bf392

--- nemu/include/cpu/instr/sub.h
+++ nemu/include/cpu/instr/sub.h
@@ -7,6 +7,7 @@ Put the declarations of `sub' instructions here.
 make_instr_func(sub_i2rm_b);
 make_instr_func(sub_r2rm_v);
 make_instr_func(sub_i2rm_bv);
+make_instr_func(sub_i2rm_v);
 make_instr_func(sub_rm2r_v);
 
 #endif

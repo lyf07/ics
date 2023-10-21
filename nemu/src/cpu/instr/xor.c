eca0682a58146ccb094f335a69f713764dac31fd
tracer-pa-public <tracer@njuics.org>
2023-10-21 17:11:03 +0800

    > compile nemu testcase kernel > id 221220160 > user pa221220160 > uname Linux 8175718b922c 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 17:11:03 up 96 days,  3:57,  4 users,  load average: 1.41, 1.74, 1.78 > unitime 1697879463601 > b038701e23bbfdbca8a9fb43b054e3847672cf89

--- nemu/src/cpu/instr/xor.c
+++ nemu/src/cpu/instr/xor.c
@@ -11,7 +11,7 @@ static void instr_execute_2op()
 // 	printf("in cmp, dest.datasize = %u\n", opr_dest.data_size);
 // 	printf("before, in cmp, src = 0x%x, dest = 0x%x\n", opr_src.val, opr_dest.val);
 // 	printf("in cmp, src.type = %i, dest.type = %i\n", opr_src.type, opr_dest.type);
-	opr_dest.val = alu_xor(sign_ext(opr_src.val, opr_src.data_size),  opr_dest.val, opr_dest.data_size);
+	opr_dest.val = alu_xor(opr_src.val,  opr_dest.val, opr_dest.data_size);
 	operand_write(&opr_dest);
 // 	printf("after, in cmp, src = 0x%x, dest = 0x%x\n", opr_src.val, opr_dest.val);
 }

b04da4b370946920ac6a5e2b0d5da707a126dea8
tracer-pa-public <tracer@njuics.org>
2023-10-21 16:22:21 +0800

    > compile nemu testcase kernel > id 221220160 > user pa221220160 > uname Linux 8175718b922c 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 16:22:21 up 96 days,  3:09,  4 users,  load average: 1.66, 1.82, 1.77 > unitime 1697876541545 > 323e40ed61d6e945bf884f07777b7dce3e93f2f

--- nemu/include/cpu/instr_helper.h
+++ nemu/include/cpu/instr_helper.h
@@ -238,9 +238,9 @@ static inline bool inv_cc()
 	    case (0x0f): {
 	        cc = instr_fetch(cpu.eip + 1, 1);
 	        switch(cc) {
+	                    case (0x86):    return cpu.eflags.CF == 1 || cpu.eflags.ZF == 1;
 	            	    case (0x8c):    return cpu.eflags.SF != cpu.eflags.OF;
 	                    case (0x8e):    return cpu.eflags.ZF == 1 || cpu.eflags.SF != cpu.eflags.OF;
-	                    case (0x95):    return cpu.eflags.ZF == 0;
 	        }
 	    }
 	}

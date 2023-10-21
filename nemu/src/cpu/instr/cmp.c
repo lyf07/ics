bf5dfbc8c4cfef90c804a99c85de20c7646b6795
tracer-pa-public <tracer@njuics.org>
2023-10-21 15:58:35 +0800

    > compile nemu testcase kernel > id 221220160 > user pa221220160 > uname Linux 8175718b922c 4.15.0-91-generic #92~16.04.1-Ubuntu SMP Fri Feb 28 14:57:22 UTC 2020 x86_64 GNU/Linux > uptime 15:58:35 up 96 days,  2:45,  4 users,  load average: 1.57, 1.58, 1.61 > unitime 1697875115242 > 2380eedf2852033f7371cf47d2bfd56e12a8af0

--- nemu/src/cpu/instr/cmp.c
+++ nemu/src/cpu/instr/cmp.c
@@ -15,6 +15,7 @@ static void instr_execute_2op()
 }
 
 make_instr_impl_2op(cmp, i, rm, bv)
+make_instr_impl_2op(cmp, i, rm, b)
 make_instr_impl_2op(cmp, r, rm, v)
 make_instr_impl_2op(cmp, i, a, b)
 make_instr_impl_2op(cmp, i, r, b)
